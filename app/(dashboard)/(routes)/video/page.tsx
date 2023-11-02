bba0e273dacda4269da4ccae4b62a17f1789f7ef
Punnoose Punnen <punnoose88@gmail.com>
2023-11-03 00:56:58 +0530

    prisma setup

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import * as z from 'zod';
+import Heading from '@/components/Heading';
+import React, { useState } from 'react';
+import { VideoIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { formSchema } from './constants';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import {
+  ChatCompletionRequestMessage,
+  ChatCompletionRequestMessageRoleEnum,
+} from 'openai';
+import Empty from '@/components/Empty';
+import Loader from '@/components/Loader';
+import { cn } from '@/lib/utils';
+import UserAvatar from '@/components/UserAvatar';
+import BotAvatar from '@/components/BotAvatar';
+
+function VideoPage() {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+      const response = await axios.post('/api/video', values);
+      setVideo(response.data[0]);
+      form.reset();
+    } catch (error: any) {
+      // Open Pro MODAl
+      console.log('mf', error);
+    } finally {
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompts into Videos"
+        icon={VideoIcon}
+        iconColor="text-orange-500"
+        bgColor="bg-orange-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid gridcols-12 gap-12"
+              onSubmit={form.handleSubmit(onSubmit)}
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Cow jumping over the moon..."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-10 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && (
+            <div>
+              <Empty label="No video Generated" />
+            </div>
+          )}
+          {video && (
+            <video
+              className="w-full aspect-video mt-8 rounded-lg border bg-black"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPage;
